697d0ada1610b9bd812931275b63be5ce965462a
JC Grennan <jgrennan94@gmail.com>
2022-08-19 15:40:41 +0100

    added img alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,15 @@ import trollFace from "../images/troll-face.png"
 // composing and exporting the Header component with JSX
 
 export default function Header() {
+
     return (
+
         <header className="header">
-            <img 
-                src={trollFace}
-                className="header--image"
-            />
+
+            <img className="header--image" src={trollFace} alt="troll face meme"/>
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course Project</h4>
+
         </header>
     )
 }
