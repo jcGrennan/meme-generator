8bcc2f0e8b9b6263324d9dff28c88308c997afef
JC Grennan <jgrennan94@gmail.com>
2022-08-19 15:35:35 +0100

    further commenting and spacing for readability

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,50 +1,75 @@
-// importing React to use State and Effect
+// importing React to use State and Effect Hooks
+
 import React from "react"
 
 // composing and exporting the Meme component
+
 export default function Meme() {
 
     // using the State hook to create an object that acts as the meme template, and a function to change that object
+
     const [meme, setMeme] = React.useState({
+
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
+
     })
 
-    // using the State hook to create an array that will hold the memes fetched from an api, an a function to change that array
+    // using the State hook to create an array that will hold the memes fetched from an api, and a function to alter that array
+
     const [allMemes, setAllMemes] = React.useState([])
     
     // using the Effect hook to fetch the memes and then setting the allMemes array to the data fetched
+
     React.useEffect(() => {
+
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
             .then(data => setAllMemes(data.data.memes))
+
     }, [])
     
-    // creating a function that chooses a random meme image from the array and sets the randomImage property to its url
+    // declaring a function that chooses a random meme image from the array and sets the randomImage property to its url
+
     function getMemeImage() {
+
         const randomNumber = Math.floor(Math.random() * allMemes.length)
         const url = allMemes[randomNumber].url
+
         setMeme(prevMeme => ({
+
             ...prevMeme,
             randomImage: url
+
         }))
         
     }
     
-    // creating a function that changes the top or bottom text property value depending on which form input has been changed
+    // declaring a function that changes the top or bottom text property value depending on which form input has been changed
+
     function handleChange(event) {
+
+        // destructuring the event target
+        
         const {name, value} = event.target
+
         setMeme(prevMeme => ({
+
             ...prevMeme,
             [name]: value
+
         }))
     }
     
-    // composing the meme generator using form inputs and the functions declared above
+    // composing the meme component using form inputs and the functions declared above
+
     return (
+
         <main>
+
             <div className="form">
+
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -53,6 +78,7 @@ export default function Meme() {
                     value={meme.topText}
                     onChange={handleChange}
                 />
+
                 <input 
                     type="text"
                     placeholder="Bottom text"
@@ -61,18 +87,22 @@ export default function Meme() {
                     value={meme.bottomText}
                     onChange={handleChange}
                 />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
+
+                <button className="form--button" onClick={getMemeImage}>
                     Get a new meme image 🖼
                 </button>
+
             </div>
+
             <div className="meme">
+
                 <img src={meme.randomImage} className="meme--image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
+
             </div>
+
         </main>
+
     )
 }
